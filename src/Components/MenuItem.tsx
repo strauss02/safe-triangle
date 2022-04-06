0177281015fbe0aec48f537344c0f398347de5cd
Ido Strauss <strauss02@hotmail.co.il>
2022-04-06 22:25:19 +0300

    conditon safe triangle usage

--- src/Components/MenuItem.tsx
+++ src/Components/MenuItem.tsx
@@ -14,48 +14,46 @@ const ArrowWrapper = styled(MenuText)`
   margin-left: 1rem;
 `
 
-const ItemWrapper = styled.div<{ hasSubmenu?: boolean }>(
-  (hasSubmenu) => css`
-    display: flex;
-    align-items: center;
-    justify-content: space-between;
-    width: auto;
-    height: 2rem;
-    padding: 0 1rem;
-    margin: 0;
-    color: black;
-    position: relative;
-    &:hover {
-      background-color: #4591d3;
-      color: white;
+const ItemWrapper = styled.div<{ hasSubmenu?: boolean }>`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  width: auto;
+  height: 2rem;
+  padding: 0 1rem;
+  margin: 0;
+  color: black;
+  position: relative;
+  &:hover {
+    background-color: #4591d3;
+    color: white;
 
-      &::after {
-        background-color: #0000ff75;
-        content: '';
-        position: absolute;
-        display: block;
-        top: 2rem;
-        left: 0rem;
-        height: 4rem; /*  2rem * submenuItems */
-        width: 100%;
-        z-index: 100;
-        clip-path: polygon(0px 0px, 100% 0px, 100% 100%);
-      }
+    ${({ hasSubmenu }) =>
+      hasSubmenu &&
+      `
+    background: blue;
+    &::after {
+      background-color: #0000ff75;
+      content: '';
+      position: absolute;
+      display: block;
+      top: 2rem;
+      left: 0rem;
+      height: 4rem; /*  2rem * submenuItems */
+      width: 100%;
+      z-index: 100;
+      clip-path: polygon(0px 0px, 100% 0px, 100% 100%);
     }
-
-    ${hasSubmenu &&
-    css`
-      background-color: blue;
-    `}
-  `
-)
+      `}
+  }
+`
 
 function MenuItem(props: menuItem) {
   const [submenuVisible, setSubmenuVisible] = useState(false)
 
   return (
     <ItemWrapper
-      hasSubmenu={false}
+      hasSubmenu={Boolean(props.subMenuItems)}
       onMouseEnter={() => {
         setSubmenuVisible(true)
       }}
@@ -64,7 +62,6 @@ function MenuItem(props: menuItem) {
       }}
     >
       <MenuText>{props.text}</MenuText>
-
       {props.subMenuItems && <ArrowWrapper>{'›'}</ArrowWrapper>}
       {props.subMenuItems && submenuVisible && (
         <Menu submenu menuGroups={[props.subMenuItems]} />
